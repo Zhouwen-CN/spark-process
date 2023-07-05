6c9fe6c3295958d2170a4ce3e3483027b52f81f1
chenzhouwen <597879949@qq.com>
2023-07-05 17:55:06 +0800

    init commit

---
+++ src/main/scala/com/yeeiee/core/transform/ExplodeTransform.scala
@@ -0,0 +1,21 @@
+package com.yeeiee.core.transform
+
+import com.yeeiee.constants.{NumberConstant, StringConstant}
+import com.yeeiee.core.env.ContextManager
+import com.yeeiee.core.transform.abs.SingleOperandTransform
+import org.apache.spark.sql.{DataFrame, functions}
+
+/**
+ * @Author: chen
+ * @Date: 2023/7/5
+ * @Desc:
+ */
+class ExplodeTransform(column: String, name: String) extends SingleOperandTransform {
+
+  override protected def realRun(context: ContextManager, operands: List[DataFrame]): DataFrame = {
+    val operand: DataFrame = operands(NumberConstant.NUMBER_0)
+    val realName: String = if (Option(name).getOrElse(StringConstant.EMPTY).nonEmpty) name else column
+
+    operand.withColumn(realName, functions.explode(functions.expr(column)))
+  }
+}
