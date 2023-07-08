0292f6c593c54a78a151d2d51a44879a9589dd19
chen0623-bak <597879949@qq.com>
2023-07-08 15:31:51 +0800

    init commit

--- src/main/scala/com/yeeiee/core/transform/UnionTransform.scala
+++ src/main/scala/com/yeeiee/core/transform/UnionTransform.scala
@@ -3,7 +3,11 @@ package com.yeeiee.core.transform
 import com.yeeiee.core.transform.abs.AssembleTransform
 import org.apache.spark.sql.DataFrame
 
-
+/**
+ * @Author: chen
+ * @Date: 2023/7/5
+ * @Desc:
+ */
 class UnionTransform(alignment: Boolean, distinct: Boolean) extends AssembleTransform(alignment, distinct) {
 
   override protected def assemble(head: DataFrame, other: DataFrame): DataFrame = {
